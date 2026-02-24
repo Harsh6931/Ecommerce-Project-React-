be94171f284d8ec735b4b090e3c0da4641d883c8
HARSHIT SINHA <harshitsinha2006@gmail.com>
2026-02-24 22:42:34 +0530

    Used axios to request backend

--- ecommerce-project-React/src/pages/Homepage.jsx
+++ ecommerce-project-React/src/pages/Homepage.jsx
@@ -1,14 +1,12 @@
+import axios from 'axios';
 import "./Homepage.css";
 import { Header } from "../components/Header"
 import { products } from "../../../starting code/data/products"
+
 export function Homepage() {
 
-  fetch('http://localhost:3000/api/products').then((response) => {
-    response.json.then((data) => {
-      console.log(data)
-
-    })
-
+  axios.get('http://localhost:3000/api/products').then((response) => {
+    console.log(response.data);
   })
   return (
     <>
