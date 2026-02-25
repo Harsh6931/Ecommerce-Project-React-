f4908c2deca17592e440b83788678d1c4a1f6eea
HARSHIT SINHA <harshitsinha2006@gmail.com>
2026-02-25 18:36:58 +0530

    The quantity radio is now functionable and connected to cart

--- ecommerce-project-React/src/pages/Homepage.jsx
+++ ecommerce-project-React/src/pages/Homepage.jsx
@@ -2,20 +2,15 @@ import axios from 'axios';
 import { useEffect,useState } from 'react';
 import "./Homepage.css";
 import { Header } from "../components/Header"
-
+import { Product } from './products.jsx';
 
 export function Homepage({cart, loadCart}) {
   const [Products,setProducts]=useState([]);
-
-
   useEffect( () => {
     axios.get('http://localhost:3000/api/products').then((response) => {
     setProducts(response.data);
   });
   },[]); 
-
-  
-
   return (
     <>
       <title>Ecommerce Site</title>
@@ -26,62 +21,8 @@ export function Homepage({cart, loadCart}) {
         <div className="products-grid">
           {Products.map((product) => {
             return (
-              <div key = {product.id} className="product-container">
-                <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
-                </div>
+              <Product product={product} loadCart={loadCart}/>
 
-                <div className="product-name limit-text-to-2-lines">
-                {product.name}
-                </div>
-
-                <div className="product-rating-container">
-                  <img
-                    className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars*10}.png`}
-                  />
-                  <div className="product-rating-count link-primary">{product.rating.count}</div>
-                </div>
-
-                <div className="product-price">${(product.priceCents/100).toFixed(2)}</div>
-
-                <div className="product-quantity-container">
-                  <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                  </select>
-                </div>
-
-                <div className="product-spacer"></div>
-
-                <div className="added-to-cart">
-                  <img src="images/icons/checkmark.png" />
-                  Added
-                </div>
-
-                <button className="add-to-cart-button button-primary"
-                onClick={async ()=> {
-                  await axios.post('http://localhost:3000/api/cart-items',{
-                    productId: product.id,
-                    quantity:1
-
-                  });
-                  await loadCart();  //update the cart on screen
-                }}>
-                  Add to Cart
-                </button>
-              </div>
             )
           })}
         </div>
